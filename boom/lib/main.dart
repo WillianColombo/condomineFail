bd365f471cbda583d82134fdb5e8860c47fcee57
Willian Colombo <williancolombo511@gmail.com>
2023-04-05 19:28:29 -0300

    Conexão do projeto com o Git

---
+++ boom/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(title: 'BOOM'),
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Este é o aplicativo BOOM, na qual será desenvolvido um mini-game campo minado com o framework Flutter em Dart',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)
+            ),
+            SizedBox(
+              width: 400,
+              height: 400,
+              child: Image.asset('assets/images/boom.png'),
+            )
+          ],
+        ),
+      ),
+    );
+    
+  }
+}
