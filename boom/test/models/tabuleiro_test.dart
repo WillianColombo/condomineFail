88fee510391702cdf6a435e3317f4d822563af32
Willian Colombo <williancolombo511@gmail.com>
2023-04-07 14:33:55 -0300

    Atualizações: Parte lógica do projeto.  - Classe campo.dart  - Classe tabuleiro.dart Testes referentes as classes citadas acima.  - Classe campo_test.dart  - Classe tabuleiro_test.dart Início interface (com problemas).

---
+++ boom/test/models/tabuleiro_test.dart
@@ -0,0 +1,21 @@
+import 'package:boom/models/tabuleiro.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Ganhar jogo', () {
+    Tabuleiro tabuleiro = Tabuleiro(
+      linhas: 2,
+      colunas: 2,
+      qtdeBombas: 0,);
+
+      tabuleiro.campos[0].minar();
+      tabuleiro.campos[3].minar();
+
+      tabuleiro.campos[0].alternarMarcacao();
+      tabuleiro.campos[1].abrir();
+      tabuleiro.campos[3].alternarMarcacao();
+      tabuleiro.campos[2].abrir();
+
+      expect(tabuleiro.resolvido, isTrue);
+  });
+}
